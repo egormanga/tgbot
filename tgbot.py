5e13c794bbf43785b94ff713823f7bd92333aa40
egormanga <egorka000111000@gmail.com>
2020-12-11 06:04:54 +0300

    Update

--- tgbot.py
+++ tgbot.py
@@ -7,10 +7,11 @@ from telegram.ext import *
 from telegram.error import *
 from utils import *; logstart('tgbot')
 
-class TGBot(metaclass=SlotsMeta):
+class TGBot(Slots):
 	""" Meant to be `@singleton'-ed. """
 
-	token: ...
+	token: None
+	queue: ...
 	updater: ...
 	dispatcher: ...
 	persistence: None
@@ -18,6 +19,7 @@ class TGBot(metaclass=SlotsMeta):
 	proxy_list_url = "http://spys.me/proxy.txt"
 
 	def __init__(self, log_filters=None):
+		if (not self.token): raise ValueError(f"{self.__name__}.token is unspecified.")
 		if (self.proxy_url is ...): self.proxy_url = self.get_proxy()
 		self.updater = Updater(token=self.token, persistence=self.persistence, request_kwargs={'proxy_url': self.proxy_url}, use_context=True)
 		self.dispatcher = self.updater.dispatcher
@@ -25,7 +27,14 @@ class TGBot(metaclass=SlotsMeta):
 		self.dispatcher.add_error_handler(self.error_callback)
 
 	def run(self):
-		self.updater.start_polling()
+		self.start()
+		self.idle()
+
+	def start(self, **kwargs):
+		self.queue = self.updater.start_polling(**kwargs)
+
+	def idle(self, **kwargs):
+		self.updater.idle(**kwargs)
 
 	@classmethod
 	def get_proxy(cls, *, timeout=1):
@@ -43,21 +52,18 @@ class TGBot(metaclass=SlotsMeta):
 	def error_callback(self, update, context):
 		logexception(context.error)
 
-	def handler(self, handler_, *args, **kwargs):
+	def handler(self, handler, *args, **kwargs):
 		@funcdecorator
 		def decorator(f):
 			def decorated(update, context):
-				scope = AttrProxy(update, context)
-				globals = f.__globals__
-				globals.update(dict(scope))
-				#globals['bot'] = self
-				globals['reply'] = lambda text, **kwargs: context.bot.send_message(chat_id=update.message.chat_id, text=text, **kwargs)
-				try: return f(update, context)
-				finally:
-					for i in scope:
-						globals.pop(i, None)
-			handler = handler_(*args, callback=decorated, **kwargs)
-			self.dispatcher.add_handler(handler)
+				def reply(text=None, message_id=None, **kwargs):
+					parseargs(kwargs, chat_id = (update.effective_chat or update.effective_user).id, message_id=message_id, text=text)
+					msg = (context.bot.edit_message_text if (message_id is not None) else context.bot.send_message)(**kwargs)
+					context.user_data['_last_message_id'] = msg.message_id
+					return msg
+				update.reply = reply
+				return f(update, context)
+			self.dispatcher.add_handler(handler(*args, callback=decorated, **kwargs))
 			return decorated
 		return decorator
 
@@ -65,10 +71,21 @@ class TGBot(metaclass=SlotsMeta):
 	def command(self, f: function): return self.command(f.__name__)(f)
 	@dispatch
 	def command(self, cmd: str, **kwargs): return self.handler(CommandHandler, cmd[1:] if (cmd.startswith('/')) else cmd, **kwargs)
+
 	@dispatch
-	def message(self, f: function): return self.message()
+	def message(self, f: function): return self.message()(f)
 	@dispatch
 	def message(self, filters=Filters.update, **kwargs): return self.handler(MessageHandler, filters, **kwargs)
+
+	@dispatch
+	def callback(self, f: function): return self.callback(rf"^{f.__name__}$")(f)
+	@dispatch
+	def callback(self, pattern, **kwargs): return self.handler(CallbackQueryHandler, pattern=pattern, **kwargs)
+
+	@dispatch
+	def command_unknown(self, f: function): return self.command_unknown()(f)
+	@dispatch
+	def command_unknown(self, filters=Filters.command, **kwargs): return self.handler(MessageHandler, filters, **kwargs)
 
 class LogHandler(MessageHandler):
 	def __init__(self, filters):
